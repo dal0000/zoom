0a6e0307079f3db891fb482528c4d46f1a5a7ae1
dal10000 <dal10000@hanmail.net>
2023-06-07 15:57:43 +0900

    camera select

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -4,17 +4,26 @@ const myFace = document.getElementById('myFace');
 
 const muteBtn = document.getElementById('mute');
 const cameraBtn = document.getElementById('camera');
+const camerasSelect = document.getElementById('cameras');
+
 let muted = false;
 let cameraOff = false;
-muteBtn.addEventListener('click',()=>{
-    muteBtn.innerText = muted? "Mute":"Unmute";
-    muted = !muted;
-});
-cameraBtn.addEventListener('click',()=>{
-    cameraBtn.innerText = cameraOff? "Camera Off":"Camera On";
-    cameraOff = !cameraOff;
-});
 let myStream;
+async function getCameras(){
+    try {
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const cameras = devices.filter(device => device.kind ==="videoinput");
+        cameras.forEach(camera =>{
+            const option = document.createElement('option');
+            option.value = camera.deviceId;
+            option.innerText = camera.label;
+            camerasSelect.appendChild(option);
+        });
+    } catch(e){
+        console.log(e);
+    }
+}
+
 async function getMedia(){
     try {
         myStream = await navigator.mediaDevices.getUserMedia({
@@ -22,9 +31,21 @@ async function getMedia(){
             video: true
         });
         myFace.srcObject = myStream;
+        await getCameras();
+        
     } catch(e){
-        const error = document.getElementById('error');
-        error.innerText = e;
+        console.log(e);
     }
 }
 getMedia();
+getCameras();
+muteBtn.addEventListener('click',()=>{
+    muteBtn.innerText = muted? "Mute":"Unmute";
+    muted = !muted;
+    myStream.getAudioTracks().forEach(track => {track.enabled = !track.enabled});
+});
+cameraBtn.addEventListener('click',()=>{
+    cameraBtn.innerText = cameraOff? "Camera Off":"Camera On";
+    cameraOff = !cameraOff;
+    myStream.getVideoTracks().forEach(track => {track.enabled = !track.enabled});
+});
