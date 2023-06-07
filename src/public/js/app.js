0151a3b822fb441dd40bda1ea794fd8d5c8dddaf
dal10000 <dal10000@hanmail.net>
2023-06-07 14:17:27 +0900

    addBtn

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -2,6 +2,18 @@ const socket = io();
 
 const myFace = document.getElementById('myFace');
 
+const muteBtn = document.getElementById('mute');
+const cameraBtn = document.getElementById('camera');
+let muted = false;
+let cameraOff = false;
+muteBtn.addEventListener('click',()=>{
+    muteBtn.innerText = muted? "Mute":"Unmute";
+    muted = !muted;
+});
+cameraBtn.addEventListener('click',()=>{
+    cameraBtn.innerText = cameraOff? "Camera Off":"Camera On";
+    cameraOff = !cameraOff;
+});
 let myStream;
 async function getMedia(){
     try {
