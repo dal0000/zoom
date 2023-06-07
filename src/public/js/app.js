f6f295828f2b260c671f1ab8c9a2c61e7f49d33b
dal10000 <dal10000@hanmail.net>
2023-06-07 13:33:56 +0900

    media

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,93 +1,19 @@
 const socket = io();
 
-const room = document.querySelector('#room');
-room.hidden = true;
+const myFace = document.getElementById('myFace');
 
-const welcome = document.querySelector('#welcome');
-const welcomeForm = welcome.querySelector('form');
-
-let roomName;
-
-function addMessage(msg){
-    const ul = room.querySelector('ul');
-    const li = document.createElement('li');
-    li.innerText = msg;
-    ul.appendChild(li);
-}
-
-function handleMessageSubmit(event){
-    event.preventDefault();
-    const input = room.querySelector('#msg input');
-    const value = input.value;
-    socket.emit("new_message", input.value, roomName, ()=>{
-        addMessage(`You: ${value}`);
-    });
-    input.value = "";
-}
-function handleNickSubmit(event){
-    event.preventDefault();
-    const input = room.querySelector('#name input');
-    socket.emit("nickname", input.value, (nick)=>{
-        const nickForm = room.querySelector('#name');
-        nickForm.hidden = true;
-        const nickSpan = room.querySelector('span');
-        nickSpan.innerText = `Nick : ${nick}`;
-    });
-}
-function showRoomName(count){
-    const h3 = room.querySelector('h3');
-    h3.innerText = `Room ${roomName} (${count})`;
-}
-function showRoom(nick, count){
-    welcome.hidden = true;
-    room.hidden = false;
-    showRoomName(count);
-    const messageForm = room.querySelector('#msg');
-    const nickForm = room.querySelector('#name');
-    messageForm.addEventListener('submit',handleMessageSubmit);
-    nickForm.addEventListener('submit',handleNickSubmit);
-    if(nick){
-        nickForm.hidden = true;
-        const nickSpan = room.querySelector('span');
-        nickSpan.innerText = `Nickname : ${nick}`;
+let myStream;
+async function getMedia(){
+    try {
+        myStream = await navigator.mediaDevices.getUserMedia({
+            audio: true,
+            video: true
+        });
+        myFace.srcObject = myStream;
+    } catch(e){
+        const error = document.getElementById('error');
+        error.innerText = e;
     }
 }
-
-function handleRoomSubmit(event){
-    event.preventDefault();
-    const roomInput = welcomeForm.querySelector('input:first-child');
-    const nickInput = welcomeForm.querySelector('input:nth-child(2)');
-    roomName = roomInput.value
-    let nickname = 'Anon';
-    if(nickInput.value){
-        nickname = nickInput.value;
-    }
-    socket.emit("enter_room", roomName, nickname, showRoom);
-}
-welcomeForm.addEventListener('submit',handleRoomSubmit);
-
-
-socket.on("welcome",(name, count)=>{
-    addMessage(`${name} joined!`);
-    showRoomName(count);
-});
-socket.on("bye",(name, count)=>{
-    addMessage(`${name} left!`);
-    showRoomName(count);
-});
-socket.on('new_message', addMessage);
-
-function showRooms(rooms){
-    const roomList = welcome.querySelector('ul');
-    roomList.innerHTML = '';
-    if(rooms.length === 0){
-        return;
-    }
-    rooms.forEach(room => {
-        const li = document.createElement('li');
-        li.innerText = room;
-        roomList.append(li);
-    });
-}
-socket.on('room_change', showRooms);
-socket.on('room',showRooms);
+getMedia();
+console.log(navigator.mediaDevices);
